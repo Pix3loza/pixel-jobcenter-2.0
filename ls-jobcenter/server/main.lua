0a76d2ea5676600ae1336dc5777a55216d1dbf89
Pixeloza <100315088+Pix3loza@users.noreply.github.com>
2024-06-17 13:35:59 +0200

    Update main.lua

--- ls-jobcenter/server/main.lua
+++ ls-jobcenter/server/main.lua
@@ -1,7 +1,22 @@
-RegisterNetEvent('ls-jobcenter:SetJob', function(data)
-    if data.job == Config.Unemployed.name or Config.Jobs[data.job] then
-        local xPlayer = ESX.GetPlayerFromId(source)
-        xPlayer.setJob(data.job, data.grade)
-    end
-end)
-
+if Config.framework == 'QB' then 
+    local QBCore = exports['qb-core']:GetCoreObject()
+    
+    RegisterNetEvent('ls-jobcenter:SetJob', function(data)
+        if data.job == Config.Unemployed.name or Config.Jobs[data.job] then
+            local src = source
+            local Player = QBCore.Functions.GetPlayer(src)
+            if Player then
+                Player.Functions.SetJob(data.job, data.grade)
+            end
+        end
+    end)
+elseif Config.framework == 'ESX' then
+    ESX = exports["es_extended"]:getSharedObject()
+    
+   RegisterNetEvent('ls-jobcenter:SetJob', function(data)
+        if data.job == Config.Unemployed.name or Config.Jobs[data.job] then
+            local xPlayer = ESX.GetPlayerFromId(source)
+            xPlayer.setJob(data.job, data.grade)
+        end
+    end) 
+end
